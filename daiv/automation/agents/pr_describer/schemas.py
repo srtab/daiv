ce7ead48455984e73b251778c277c8566bad0d1e
Sandro <sandro.rodrigues@dipcode.com>
2024-10-23 17:07:07 +0100

    Improved definition of branch naming.

--- daiv/automation/agents/pr_describer/schemas.py
+++ daiv/automation/agents/pr_describer/schemas.py
@@ -2,8 +2,14 @@ from pydantic import BaseModel, Field
 
 
 class PullRequestDescriberOutput(BaseModel):
-    branch: str = Field(description="The branch name.")
-    title: str = Field(description="Title with a short and concise description of the changes.")
+    branch: str = Field(
+        description=(
+            "The branch name. No spaces are allowed in branch names. "
+            "You can use numbers, hyphens (-), underscores (_), lowercase letters from the ASCII standard table "
+            "or forward slashes (/)."
+        )
+    )
+    title: str = Field(description="Title with no more than 10 words and concise description of the changes.")
     description: str = Field(description=("Description of the functional changes."))
     summary: list[str] = Field(
         description=(
