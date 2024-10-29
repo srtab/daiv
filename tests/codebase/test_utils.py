1b340b707e6828170bf91bd28dcb178c755ea3ec
Sandro <sandro.rodrigues@dipcode.com>
2024-10-29 11:13:57 +0000

    Fixed unittests.

--- tests/codebase/test_utils.py
+++ tests/codebase/test_utils.py
@@ -1,8 +1,6 @@
-from unittest.mock import Mock
-
 from langchain_core.messages import AIMessage, HumanMessage
 
-from codebase.base import Note, NoteableType
+from codebase.base import Note, NoteableType, User
 from codebase.utils import notes_to_messages
 from core.constants import BOT_NAME
 
@@ -12,7 +10,7 @@ class TestNotesToMessages:
         bot_user_id = 1
         notes = [
             Note(
-                author=Mock(id=1),
+                author=User(id=1, name="bot", username="bot"),
                 body="Bot message 1",
                 id=1,
                 noteable_type=NoteableType.ISSUE,
@@ -20,7 +18,7 @@ class TestNotesToMessages:
                 resolvable=True,
             ),
             Note(
-                author=Mock(id=1),
+                author=User(id=1, name="bot", username="bot"),
                 body="Bot message 2",
                 id=2,
                 noteable_type=NoteableType.ISSUE,
@@ -29,8 +27,8 @@ class TestNotesToMessages:
             ),
         ]
         expected_messages = [
-            AIMessage(content="Bot message 1", name=BOT_NAME),
-            AIMessage(content="Bot message 2", name=BOT_NAME),
+            AIMessage(content="Bot message 1", name=BOT_NAME, id=1),
+            AIMessage(content="Bot message 2", name=BOT_NAME, id=2),
         ]
         result = notes_to_messages(notes, bot_user_id)
         assert result == expected_messages
@@ -39,7 +37,7 @@ class TestNotesToMessages:
         bot_user_id = 1
         notes = [
             Note(
-                author=Mock(id=2, username="user1"),
+                author=User(id=2, username="user1", name="user1"),
                 body="User message 1",
                 id=1,
                 noteable_type=NoteableType.ISSUE,
@@ -47,7 +45,7 @@ class TestNotesToMessages:
                 resolvable=True,
             ),
             Note(
-                author=Mock(id=3, username="user2"),
+                author=User(id=3, username="user2", name="user1"),
                 body="User message 2",
                 id=2,
                 noteable_type=NoteableType.ISSUE,
@@ -56,8 +54,8 @@ class TestNotesToMessages:
             ),
         ]
         expected_messages = [
-            HumanMessage(content="User message 1", name="user1"),
-            HumanMessage(content="User message 2", name="user2"),
+            HumanMessage(content="User message 1", name="user1", id=1),
+            HumanMessage(content="User message 2", name="user2", id=2),
         ]
         result = notes_to_messages(notes, bot_user_id)
         assert result == expected_messages
@@ -66,7 +64,7 @@ class TestNotesToMessages:
         bot_user_id = 1
         notes = [
             Note(
-                author=Mock(id=1),
+                author=User(id=1, username="bot", name="bot"),
                 body="Bot message",
                 id=1,
                 noteable_type=NoteableType.ISSUE,
@@ -74,7 +72,7 @@ class TestNotesToMessages:
                 resolvable=True,
             ),
             Note(
-                author=Mock(id=2, username="user1"),
+                author=User(id=2, username="user1", name="user1"),
                 body="User message",
                 id=2,
                 noteable_type=NoteableType.ISSUE,
@@ -83,8 +81,8 @@ class TestNotesToMessages:
             ),
         ]
         expected_messages = [
-            AIMessage(content="Bot message", name=BOT_NAME),
-            HumanMessage(content="User message", name="user1"),
+            AIMessage(content="Bot message", name=BOT_NAME, id=1),
+            HumanMessage(content="User message", name="user1", id=2),
         ]
         result = notes_to_messages(notes, bot_user_id)
         assert result == expected_messages
