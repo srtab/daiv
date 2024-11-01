c037fd8da571041fd6d4d0296aed88d6941baca0
Sandro <sandro.rodrigues@dipcode.com>
2024-11-01 18:47:20 +0000

    Fixed max_token on replacer agent.

--- daiv/automation/agents/base.py
+++ daiv/automation/agents/base.py
@@ -2,11 +2,13 @@ from __future__ import annotations
 
 from abc import ABC, abstractmethod
 from decimal import Decimal
-from enum import Enum
+from enum import StrEnum
 from typing import TYPE_CHECKING, Generic, TypeVar, cast
 
 from langchain.chat_models.base import BaseChatModel, _attempt_infer_model_provider, init_chat_model
+from langchain_anthropic.chat_models import ChatAnthropic
 from langchain_community.callbacks import OpenAICallbackHandler
+from langchain_core.messages import BaseMessage, get_buffer_string
 from langchain_core.runnables import Runnable, RunnableConfig
 from langchain_openai.chat_models import ChatOpenAI
 from pydantic import BaseModel
@@ -22,7 +24,7 @@ GENERIC_PERFORMANT_MODEL_NAME = "gpt-4o-2024-08-06"
 GENERIC_COST_EFFICIENT_MODEL_NAME = "gpt-4o-mini-2024-07-18"
 
 
-class ModelProvider(Enum):
+class ModelProvider(StrEnum):
     ANTHROPIC = "anthropic"
     OPENAI = "openai"
 
@@ -107,6 +109,32 @@ class BaseAgent(ABC, Generic[T]):
         """
         return self.agent.get_graph().draw_mermaid()
 
+    def get_num_tokens(self, text: str) -> int:
+        """
+        Get the number of tokens in a text.
+
+        Args:
+            text (str): The text
+
+        Returns:
+            int: The number of tokens
+        """
+        if _attempt_infer_model_provider(self.model_name) == ModelProvider.ANTHROPIC:
+            return cast(ChatAnthropic, self.model)._client.count_tokens(text)
+        return self.model.get_num_tokens(text)
+
+    def get_num_tokens_from_messages(self, messages: list[BaseMessage]) -> int:
+        """
+        Get the number of tokens from a list of messages.
+
+        Args:
+            messages (list[BaseMessage]): The messages
+
+        Returns:
+            int: The number of tokens
+        """
+        return sum(self.get_num_tokens(get_buffer_string([m])) for m in messages)
+
     def get_max_token_value(self) -> int:
         """
         Get the maximum token value for the model.
@@ -114,6 +142,7 @@ class BaseAgent(ABC, Generic[T]):
         Returns:
             int: The maximum token value
         """
+
         match _attempt_infer_model_provider(self.model_name):
             case ModelProvider.ANTHROPIC:
                 return 8192 if self.model_name.startswith("claude-3-5-sonnet") else 4096
