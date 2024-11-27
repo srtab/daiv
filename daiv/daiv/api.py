d0dca95f2326485f00b7d757696054ef6816037f
Sandro <sandro.rodrigues@dipcode.com>
2024-11-27 10:40:02 +0000

    Reverted api code.

--- daiv/daiv/api.py
+++ daiv/daiv/api.py
@@ -1,8 +1,6 @@
-from chat.api.views import router as chat_router
 from ninja import NinjaAPI
 
-from codebase.api.views import router as codebase_router
+from codebase.api.views import router
 
 api = NinjaAPI()
-api.add_router("/codebase", codebase_router)
-api.add_router("/v1", chat_router)
+api.add_router("/codebase", router)
