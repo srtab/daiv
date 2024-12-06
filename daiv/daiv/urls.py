d3211ad12f4883161a6855e98e76cd0f1e8c4aff
Sandro <sandro.rodrigues@dipcode.com>
2024-12-06 12:22:18 +0000

    Added alivew view.

--- daiv/daiv/urls.py
+++ daiv/daiv/urls.py
@@ -1,9 +1,13 @@
 from django.conf import settings
 from django.urls import path
 
+from core.views import HealthCheckView
 from daiv.api import api
 
-urlpatterns = [path(route="api/", view=api.urls)]
+urlpatterns = [
+    path(route="api/", view=api.urls),
+    path(route="-/alive/", view=HealthCheckView.as_view(), name="health_check"),
+]
 
 if settings.DEBUG:  # pragma: no cover
     from django.conf.urls.static import static
