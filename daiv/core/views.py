dd769e3fdf074c9f0a55f90a279cc41e1c01518d
Sandro <sandro.rodrigues@dipcode.com>
2024-12-06 12:22:18 +0000

    Added alivew view.

---
+++ daiv/core/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+from django.views import View
+
+
+class HealthCheckView(View):
+    """
+    Simple health check endpoint that returns 200 OK.
+    """
+
+    def get(self, request, *args, **kwargs):
+        return HttpResponse("OK", content_type="text/plain")
