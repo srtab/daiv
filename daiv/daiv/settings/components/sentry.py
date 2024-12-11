7ba5e420928ea96c662f9304f0eddbc1a95d33d8
Sandro <sandro.rodrigues@dipcode.com>
2024-12-11 23:47:47 +0000

    Added support to Sentry communicate using custom CA certificates.

--- daiv/daiv/settings/components/sentry.py
+++ daiv/daiv/settings/components/sentry.py
@@ -9,6 +9,7 @@ from daiv.settings.components import ENVIRONMENT
 SENTRY_DSN = get_docker_secret("SENTRY_DSN")
 SENTRY_DEBUG = config("SENTRY_DEBUG", cast=bool, default=False)
 SENTRY_ENABLE_TRACING = config("SENTRY_ENABLE_TRACING", cast=bool, default=False)
+SENTRY_CA_CERTS = config("SENTRY_CA_CERTS", cast=str, default=None)
 
 if SENTRY_DSN:
     import sentry_sdk
@@ -26,6 +27,7 @@ if SENTRY_DSN:
         enable_tracing=SENTRY_ENABLE_TRACING,
         profiles_sample_rate=1.0 if SENTRY_ENABLE_TRACING else 0.0,
         server_name=config("NODE_HOSTNAME", default=None),
+        ca_certs=SENTRY_CA_CERTS,
         integrations=[
             DjangoIntegration(),
             LoggingIntegration(),
