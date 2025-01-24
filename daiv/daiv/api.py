9e71bd0bbf94ed9f7b1317d38efca6b884696099
Sandro Rodrigues <srtabs@gmail.com>
2025-01-24 23:38:41 +0000

    Improved README. (#211)

--- daiv/daiv/api.py
+++ daiv/daiv/api.py
@@ -5,7 +5,7 @@ from codebase.api.views import router as codebase_router
 
 from . import __version__
 
-api = NinjaAPI(version=__version__, title="Daiv API")
+api = NinjaAPI(version=__version__, title="Daiv API", docs_url="/docs/")
 api.add_router("/codebase", codebase_router)
 api.add_router("/chat", chat_router)
 api.add_router("/models", models_router)
