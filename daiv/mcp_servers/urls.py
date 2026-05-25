668506e25552c48ed703e8149c4524e94501b2e6
Sandro <sandro.rodrigues@dipcode.com>
2026-05-25 19:24:51 +0100

    feat(mcp_servers): add edit view with built-in restrictions

--- daiv/mcp_servers/urls.py
+++ daiv/mcp_servers/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from mcp_servers.views import MCPServerCreateView, MCPServerListView
+from mcp_servers.views import MCPServerCreateView, MCPServerEditView, MCPServerListView
 
 app_name = "mcp_servers"
 
 urlpatterns = [
     path("", MCPServerListView.as_view(), name="list"),
     path("new/", MCPServerCreateView.as_view(), name="create"),
+    path("<slug:name>/edit/", MCPServerEditView.as_view(), name="edit"),
 ]
