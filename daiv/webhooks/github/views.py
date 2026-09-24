0a49cfba84bde5df80d0ee8f243a9f39a4a20ff6
Sandro <sandro.rodrigues@dipcode.com>
2026-09-24 22:15:12 +0100

    style(webhooks): restore the github views import grouping

--- daiv/webhooks/github/views.py
+++ daiv/webhooks/github/views.py
@@ -5,13 +5,7 @@ from codebase.base import GitPlatform
 from codebase.conf import settings
 from webhooks.callbacks import UnprocessableEntityResponse
 
-from .callbacks import (  # noqa: TC001
-    IssueCallback,
-    IssueCommentCallback,
-    PullRequestCallback,
-    PushCallback,
-    WorkflowRunCallback,
-)
+from .callbacks import IssueCallback, IssueCommentCallback, PullRequestCallback, PushCallback, WorkflowRunCallback  # noqa: TC001
 from .security import validate_github_webhook
 
 logger = logging.getLogger("daiv.webhooks")
